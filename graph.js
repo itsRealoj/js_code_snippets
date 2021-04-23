9c6486a348117a4a89ce037ac122ec69fd248751
itsrealoj <stephenojwang1040@gmail.com>
2021-04-23 06:55:23 +0300

    add vertex to graph class

--- graph.js
+++ graph.js
@@ -1,12 +1,25 @@
 class Graph {
-    constructor() {
-        this.adjacencyList = {}
-    }
-    addVertex(vertex){
-        this.adjacencyList[vertex] = []
-    }
+ constructor(){
+     this.adjacencyList = {};
+ }
+
+ addVertex(vertex) {
+     this.adjacencyList[vertex] = [];
+ }
+
+ addEdge(edge, val){
+    this.adjacencyList[edge].push(val)
+ }
+
 }
 
-const g = new Graph()
-g.addVertex('USA')
+const g = new Graph();
+g.addVertex('MON')
+g.addVertex('TUE')
+g.addVertex('WED')
+
+g.addEdge('TUE', 2)
+g.addEdge('MON', 10)
+
 console.log(g)
+console.log(g.addVertex)
