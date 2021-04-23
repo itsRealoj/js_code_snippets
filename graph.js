db5e2c41b8dc215bba378269abf753e6f0c0c3bf
itsrealoj <stephenojwang1040@gmail.com>
2021-04-23 06:05:44 +0300

    constructor class

---
+++ graph.js
@@ -0,0 +1,11 @@
+class Graph {
+    constructor(name) {
+        this.addVertex = () => {
+            name = []
+        }
+        // return this
+    }
+}
+
+// console.log(new Graph('john'))
+    console.log(new Graph('A'))
